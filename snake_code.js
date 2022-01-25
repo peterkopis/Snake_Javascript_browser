7634a5752e317f175cdcee0cd24714c4a4ad0bd4
Peter <peter.pis@centrum.sk>
2022-01-25 09:58:02 +0100

    repare the pause of game

--- snake_code.js
+++ snake_code.js
@@ -3,13 +3,12 @@ let cntx=canvas.getContext("2d");
 let rows= 20;
 let columns= 20;
 let snake= [
-    {x:15,y:3}
-    
-];
+             {x:15,y:3}   
+            ];
 let food;
 let cellwidth =canvas.width/columns;
 let cellheight= canvas.height/rows; 
-let direccion ;
+let direccion;
 let foodColected = false;
 let TryGameOver = false;
 let pause = false;
@@ -18,7 +17,7 @@ let previousDireccion;
 document.addEventListener("keydown", keyDown);
 
 setInterval(gameloop,500);
-//setInterval(console.log(snake[0].x),1000);
+
 placeFood();
 
 draw();
@@ -30,10 +29,7 @@ function draw()
     cntx.fillStyle="white";
     snake.forEach(part=>addRect(part.x,part.y));
     
-    /*addRect(40,20);
-    addRect(40,40);
-    addRect(40,60);
-    addRect(40,80);*/
+   
     cntx.fillStyle="yellow";
     addRect(food.x,food.y);
     requestAnimationFrame(draw);
@@ -45,30 +41,37 @@ function draw()
     
     if(pause)
     { 
-       console.log('pause');
+      
         cntx.fillStyle = "white";
         cntx.fillText("Pause", (canvas.width / 2), 
-    (canvas.height / 2) );}
+                        (canvas.height / 2) );
+    }
     
     
 };
-function addRect(w,h){
+function addRect(w,h)
+{
     cntx.fillRect(w*cellwidth,h*cellheight,
         cellwidth-1,cellheight-1);
 };
-function shiftsnake(){
-
-    for(let i= snake.length-1; i>0;i--){
-        let part = snake[i];
-        let lastpart = snake[i-1];
-        part.x = lastpart.x;
-        part.y =lastpart.y;
+function shiftsnake()
+{
+    if(direccion != null)
+    {
+        for(let i= snake.length-1; i>0;i--)
+        {
+            let part = snake[i];
+            let lastpart = snake[i-1];
+            part.x = lastpart.x;
+            part.y =lastpart.y;
+        }
     }
-}
+};
  
 function gameloop()
 {
-    if(foodColected){
+    if(foodColected)
+    {
         snake = [
             {x: snake[0].x,
             y: snake[0].y},
@@ -78,21 +81,22 @@ function gameloop()
     };
     if(TryGameOver == false)
     {
-    shiftsnake();
+        shiftsnake();
     };
     if(TryGameOver == false &&
         pause == false)
     {
-    if(direccion=="LEFT") {snake[0].x--; 
-        previousDireccion = "LEFT"};
-    if(direccion=="UP") {snake[0].y--;
-        previousDireccion = "UP"};
-    if(direccion=="RIGHT") {snake[0].x++;
-        previousDireccion = "RIGHT"};
-    if(direccion=="DOWN") {snake[0].y++;
-        previousDireccion = "DOWN"};
+    if(direccion=="LEFT") 
+    { snake[0].x--; previousDireccion = "LEFT"};
+    if(direccion=="UP") 
+    { snake[0].y--; previousDireccion = "UP"};
+    if(direccion=="RIGHT") 
+    {snake[0].x++; previousDireccion = "RIGHT"};
+    if(direccion=="DOWN") 
+    {snake[0].y++; previousDireccion = "DOWN"};
     if(snake[0].x == food.x 
-        && snake[0].y ==food.y){
+        && snake[0].y ==food.y)
+        {
             foodColected = true;
             placeFood();
 
@@ -102,13 +106,15 @@ function gameloop()
 
 };
 
-function keyDown(e){
+function keyDown(e)
+{
     if(e.keyCode == 37){direccion="LEFT"};
     if(e.keyCode == 38){direccion="UP"}; 
     if(e.keyCode == 39){direccion="RIGHT"};
     if(e.keyCode == 40){direccion="DOWN"};
-}
-function placeFood(){
+};
+function placeFood()
+{
     let randomX= Math.floor(Math.random()*columns);
     let randomY= Math.floor(Math.random()*rows);
     food= {
@@ -135,6 +141,7 @@ function gameOver()
        {
         TryGameOver = true;
         direccion = null;
+        pause = false;
         
        }
 };
@@ -143,6 +150,7 @@ function newstart()
     placeFood();
     foodColected = false;
     TryGameOver = false;
+    pause = false;
      snake= [
         {x:15,y:3}
         
@@ -152,11 +160,14 @@ function newstart()
 function pausedGame()
 {
     if(pause ==true)
-    {pause = false;
-     direccion =previousDireccion; }
-    else{
-        direccion = null;
+    {
+        pause = false;
+     direccion = previousDireccion;
+     }
+    else
+    {
         pause = true;
+        direccion = null;
     };
     
 }
