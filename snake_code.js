4a3317b8f9052bcc67105c7916c5b8a3d9cbb3f9
Peter <peter.pis@centrum.sk>
2022-01-26 11:22:34 +0100

    add the coments and rename one variable and write better last repare

--- snake_code.js
+++ snake_code.js
@@ -1,174 +1,175 @@
-let canvas= document.getElementById("canvas");
-let cntx=canvas.getContext("2d");
-let rows= 20;
-let columns= 20;
-let snake= [
-             {x:15,y:3}   
-            ];
+let canvas = document.getElementById("canvas");
+let cntx = canvas.getContext("2d");
+let rows = 20;
+let columns = 20;
+let snake = [
+    { x: 15, y: 3 }
+];
 let food;
-let cellwidth =canvas.width/columns;
-let cellheight= canvas.height/rows; 
+let cellwidth = canvas.width / columns;
+let cellheight = canvas.height / rows;
 let direccion;
 let foodColected = false;
-let TryGameOver = false;
+let tryGameOver = false;
 let pause = false;
 let previousDireccion;
-
+//listener for control the game 
 document.addEventListener("keydown", keyDown);
-
-setInterval(gameloop,500);
+//speed of game
+setInterval(gameloop, 500);
 
 placeFood();
 
 draw();
 
-function draw()
-{
-    cntx.fillStyle= "black";
-    cntx.fillRect(0,0,canvas.width,canvas.height);
-    cntx.fillStyle="white";
-    snake.forEach(part=>addRect(part.x,part.y));
-    
-   
-    cntx.fillStyle="yellow";
-    addRect(food.x,food.y);
+function draw() {
+    //matchfield
+    cntx.fillStyle = "black";
+    cntx.fillRect(0, 0, canvas.width, canvas.height);
+    // our snake
+    cntx.fillStyle = "white";
+    snake.forEach(part => addRect(part.x, part.y));
+
+    // our food
+    cntx.fillStyle = "yellow";
+    addRect(food.x, food.y);
+    //requests that the browser call a specified function to update an animation before the next repaint
     requestAnimationFrame(draw);
-    if(TryGameOver){
+    // if the game is over, write on matchfeld gameover
+    if (tryGameOver) {
         cntx.fillStyle = "white";
         cntx.fillText("Game Over", (canvas.width / 2),
-         (canvas.height / 2) );
+            (canvas.height / 2));
     };
-    
-    if(pause)
-    { 
-      
+    // if game is paused, write on matchfeld pause
+    if (pause) {
+
         cntx.fillStyle = "white";
-        cntx.fillText("Pause", (canvas.width / 2), 
-                        (canvas.height / 2) );
+        cntx.fillText("Pause", (canvas.width / 2),
+            (canvas.height / 2));
     }
-    
-    
+
+
 };
-function addRect(w,h)
-{
-    cntx.fillRect(w*cellwidth,h*cellheight,
-        cellwidth-1,cellheight-1);
+// add new rectangle
+function addRect(w, h) {
+    cntx.fillRect(w * cellwidth, h * cellheight,
+        cellwidth - 1, cellheight - 1);
 };
-function shiftsnake()
-{
-    if(direccion != null)
-    {
-        for(let i= snake.length-1; i>0;i--)
-        {
-            let part = snake[i];
-            let lastpart = snake[i-1];
-            part.x = lastpart.x;
-            part.y =lastpart.y;
-        }
+// function for shifting the body of the snake
+function shiftsnake() {
+    for (let i = snake.length - 1; i > 0; i--) {
+        let part = snake[i];
+        let lastpart = snake[i - 1];
+        part.x = lastpart.x;
+        part.y = lastpart.y;
     }
 };
- 
-function gameloop()
-{
-    if(foodColected)
-    {
-        snake = [
-            {x: snake[0].x,
-            y: snake[0].y},
+
+function gameloop() {
+    // if is food colected, the snake grow
+    if (foodColected) {
+        snake = [{
+                x: snake[0].x,
+                y: snake[0].y
+            },
             ...snake
         ];
         foodColected = false;
     };
-    if(TryGameOver == false)
-    {
+    // shift the body of snake , only if game isn´t paused or over
+    if (tryGameOver == false &&
+        pause == false) {
         shiftsnake();
     };
-    if(TryGameOver == false &&
-        pause == false)
-    {
-    if(direccion=="LEFT") 
-    { snake[0].x--; previousDireccion = "LEFT"};
-    if(direccion=="UP") 
-    { snake[0].y--; previousDireccion = "UP"};
-    if(direccion=="RIGHT") 
-    {snake[0].x++; previousDireccion = "RIGHT"};
-    if(direccion=="DOWN") 
-    {snake[0].y++; previousDireccion = "DOWN"};
-    if(snake[0].x == food.x 
-        && snake[0].y ==food.y)
-        {
+    // permite control the snake, only if game isn´t paused or over, in variable  previousDireccion remebrer the previous direccion, if is game paused
+    if (tryGameOver == false &&
+        pause == false) {
+        if (direccion == "LEFT") {
+            snake[0].x--;
+            previousDireccion = "LEFT"
+        };
+        if (direccion == "UP") {
+            snake[0].y--;
+            previousDireccion = "UP"
+        };
+        if (direccion == "RIGHT") {
+            snake[0].x++;
+            previousDireccion = "RIGHT"
+        };
+        if (direccion == "DOWN") {
+            snake[0].y++;
+            previousDireccion = "DOWN"
+        };
+        // try if has the snake food colected
+        if (snake[0].x == food.x &&
+            snake[0].y == food.y) {
             foodColected = true;
             placeFood();
 
         };
     }
-  gameOver();      
+    gameOver();
 
 };
 
-function keyDown(e)
-{
-    if(e.keyCode == 37){direccion="LEFT"};
-    if(e.keyCode == 38){direccion="UP"}; 
-    if(e.keyCode == 39){direccion="RIGHT"};
-    if(e.keyCode == 40){direccion="DOWN"};
+function keyDown(e) {
+    if (e.keyCode == 37) { direccion = "LEFT" };
+    if (e.keyCode == 38) { direccion = "UP" };
+    if (e.keyCode == 39) { direccion = "RIGHT" };
+    if (e.keyCode == 40) { direccion = "DOWN" };
 };
-function placeFood()
-{
-    let randomX= Math.floor(Math.random()*columns);
-    let randomY= Math.floor(Math.random()*rows);
-    food= {
-        x:randomX,
-        y:randomY
+// random place of food
+function placeFood() {
+    let randomX = Math.floor(Math.random() * columns);
+    let randomY = Math.floor(Math.random() * rows);
+    food = {
+        x: randomX,
+        y: randomY
     };
 
 };
-function gameOver()
-{ 
+
+function gameOver() {
     let firstPart = snake[0];
     let othersPart = snake.slice(1);
-    let duplicatedPart = othersPart.find(part => 
+    let duplicatedPart = othersPart.find(part =>
         part.x == firstPart.x &&
         part.y == firstPart.y
-        );
-    if(
+    );
+    if (
         snake[0].x < 0 ||
-        snake[0].x > columns-1||
+        snake[0].x > columns - 1 ||
         snake[0].y < 0 ||
-        snake[0].y > rows-1  ||
+        snake[0].y > rows - 1 ||
         duplicatedPart
-       )
-       {
-        TryGameOver = true;
+    ) {
+        tryGameOver = true;
         direccion = null;
         pause = false;
-        
-       }
+
+    }
 };
-function newstart()
-{
+
+function newstart() {
     placeFood();
     foodColected = false;
-    TryGameOver = false;
+    tryGameOver = false;
     pause = false;
-     snake= [
-        {x:15,y:3}
-        
+    snake = [
+        { x: 15, y: 3 }
+
     ];
-   
+
 }
-function pausedGame()
-{
-    if(pause ==true)
-    {
+
+function pausedGame() {
+    if (pause == true) {
         pause = false;
-     direccion = previousDireccion;
-     }
-    else
-    {
+        direccion = previousDireccion;
+    } else {
         pause = true;
         direccion = null;
     };
-    
-}
 
+}
